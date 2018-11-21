837574ae0b6e2bfc3f12b18803f9077bb7a49762
Leen Kim <leenkim@alumni.usc.edu>
2018-11-21 01:48:28 -0800

    add css styling in js

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? "100px" : "50px")};
+  /* font-size: 50px; */
+  span {
+    font-size: 100px;
+  }
+`;
+
+// const BigPoop = styled.span`
+// font-size: 100px;
+// `;
 
 class Page extends Component {
   render() {
@@ -8,6 +22,16 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge>
+          Click Me
+          {/* <BigPoop>💩</BigPoop> */}
+          <span>💩</span>
+        </MyButton>
+        <Header />
+        <MyButton>
+          Click Me
+          <span>💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     );
