2e3894d02d8112d3ee4e1945a48d42390f8f2835
Leen Kim <leenkim@alumni.usc.edu>
2018-11-26 21:16:08 -0800

    completed 22 Pagination component and page

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   items: forwardTo("db"),
-  item: forwardTo("db")
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
   // When it is the same in prisma and yoga, then you can just forwardTo("db")
 
   // async items(parent, args, ctx, info) {
